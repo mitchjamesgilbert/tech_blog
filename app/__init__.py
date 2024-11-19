c9bbd526275aed963f835d2e06b3ae5c68e197cc
mitch <mitch.james.gilbert@gmail.com>
2024-11-19 02:09:49 +0000

    Add initial SQLite logic

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    db.init_app(app)
+
+    with app.app_context():
+        from app.models import BlogPost
+        db.create_all()
 
     from .routes import bp as routes_bp
     app.register_blueprint(routes_bp)
---
+++ app/models.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from . import db
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    tags = db.Column(db.String(200), nullable=True)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return f"<BlogPost {self.title}>"
+    
+    def set_tags(self, tags_list):
+        self.tags = ",".join(tags_list)
+    
+    def get_tags(self):
+        return self.tags.split(",") if self.tags else []
